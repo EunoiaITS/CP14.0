e8dd33e2aaf14fde8d71b061e082bc4025224e8e
Toki EIS <35416718+Toki-EIS@users.noreply.github.com>
2018-08-15 18:55:24 +0600

    admin logout fix

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -10,7 +10,10 @@
                 <p>Are you sure you want to logout ?</p>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Yes</button>
+                <form method="post" action="{{ url('/logout') }}" id="logout">
+                    {{ csrf_field() }}
+                </form>
+                <button type="submit" form="logout" class="btn btn-primary">Yes</button>
                 <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
             </div>
         </div>
