d417cf6ae11a098f9eb6ba41bd4362f32825541d
Mahmudul-EIS <35486228+Mahmudul-EIS@users.noreply.github.com>
2018-12-12 10:27:37 +0600

    update about us content

--- resources/views/frontend/pages/about-us.blade.php
+++ resources/views/frontend/pages/about-us.blade.php
@@ -8,16 +8,15 @@
                 <h3 class="get-about-section-header section-about-us-top">About Us</h3>
                 <div class="about-us-details">
                     <h4 class="about-us-details-title">About GetWobo</h4>
-                    <p>Vitae feugiat iaculis pulvinar justo cursus luctus donec taciti nascetur ligula habitasse metus rhoncus vulputate tellus ullamcorper. Ultricies sociis risus semper per convallis. Adipiscing mi felis ad urna pellentesque a per pharetra Praesent vitae. Quis pellentesque. Placerat cras fermentum phasellus malesuada habitasse torquent taciti posuere Id ornare Egestas suscipit. <br>
-                        Ad mattis augue sociosqu sollicitudin elementum semper iaculis aenean praesent aliquet class felis sem viverra pharetra class enim nullam nibh felis mus. Viverra malesuada elit. Morbi velit fermentum, enim condimentum tellus ipsum neque eleifend in aenean rutrum suscipit etiam sit nisi cubilia velit eros nulla diam id. Montes conubia inceptos tellus penatibus sagittis lectus pharetra magnis. Rhoncus nonummy fermentum a in eget sapien cras. </p>
+                    <p>getWOBO offers shared-ride opportunity in effort to resolve logistic challenges from metropolitan to countryside villages. The current world population is recorded at 7.4 billion. 50% from world population are either commuters (Riders) or drivers (Ridemates). These group of population may either have a need for logistic, or able to offer logistic solutions. This is people powered platform and the easiest way to overcome logistic challenges around the globe.</p>
                 </div>
                 <div class="about-us-details">
                     <h4 class="about-us-details-title">Our Mission</h4>
-                    <p>Vitae feugiat iaculis pulvinar justo cursus luctus donec taciti nascetur ligula habitasse metus rhoncus vulputate tellus ullamcorper. Ultricies sociis risus semper per convallis. Adipiscing mi felis ad urna pellentesque a per pharetra Praesent vitae. Quis pellentesque. Placerat cras fermentum phasellus malesuada habitasse torquent taciti posuere Id ornare Egestas suscipit.</p>
+                    <p>getWOBO’s mission is to create a flawless logistic world by connecting to local cultures and having unique commuting experience. Its community marketplace provides access to millions of unique Ride Services, strongly tie to our fundamental believe: <i>Sharing is Caring</i>.</p>
                 </div>
                 <div class="about-us-details">
-                    <h4 class="about-us-details-title">Your Ride anytime anywhere</h4>
-                    <p>Vitae feugiat iaculis pulvinar justo cursus luctus donec taciti nascetur ligula habitasse metus rhoncus vulputate tellus ullamcorper. Ultricies sociis risus semper per convallis. Adipiscing mi felis ad urna pellentesque a per pharetra Praesent vitae. Quis pellentesque. Placerat cras fermentum phasellus malesuada habitasse torquent taciti posuere Id ornare Egestas suscipit.</p>
+                    <h4 class="about-us-details-title">Your Ride, anytime, anywhere</h4>
+                    <p>As a platform, we continue to develop services to help to meet the ever increasing logistic demands, as well as creating opportunities on global scale.</p>
                 </div>
             </div>
         </div>
