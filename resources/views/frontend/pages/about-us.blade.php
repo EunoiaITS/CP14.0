1f5a1a0477f10b88370b9b2e8a382946bd6515a0
Mahmudul-EIS <35486228+Mahmudul-EIS@users.noreply.github.com>
2019-03-30 15:36:17 +0600

    Update about-us.blade.php

--- resources/views/frontend/pages/about-us.blade.php
+++ resources/views/frontend/pages/about-us.blade.php
@@ -7,12 +7,12 @@
             <div class="col-sm-12">
                 <h3 class="get-about-section-header section-about-us-top">About Us</h3>
                 <div class="about-us-details">
-                    <h4 class="about-us-details-title">About GetWobo</h4>
-                    <p>getWOBO offers shared-ride opportunity in effort to resolve logistic challenges from metropolitan to countryside villages. The current world population is recorded at 7.4 billion. 50% from world population are either commuters (Riders) or drivers (Ridemates). These group of population may either have a need for logistic, or able to offer logistic solutions. This is people powered platform and the easiest way to overcome logistic challenges around the globe.</p>
+                    <h4 class="about-us-details-title">About carhailing</h4>
+                    <p>carhailing offers shared-ride opportunity in effort to resolve logistic challenges from metropolitan to countryside villages. The current world population is recorded at 7.4 billion. 50% from world population are either commuters (Riders) or drivers (Ridemates). These group of population may either have a need for logistic, or able to offer logistic solutions. This is people powered platform and the easiest way to overcome logistic challenges around the globe.</p>
                 </div>
                 <div class="about-us-details">
                     <h4 class="about-us-details-title">Our Mission</h4>
-                    <p>getWOBO’s mission is to create a flawless logistic world by connecting to local cultures and having unique commuting experience. Its community marketplace provides access to millions of unique Ride Services, strongly tie to our fundamental believe: <i>Sharing is Caring</i>.</p>
+                    <p>carhailing’s mission is to create a flawless logistic world by connecting to local cultures and having unique commuting experience. Its community marketplace provides access to millions of unique Ride Services, strongly tie to our fundamental believe: <i>Sharing is Caring</i>.</p>
                 </div>
                 <div class="about-us-details">
                     <h4 class="about-us-details-title">Your Ride, anytime, anywhere</h4>
